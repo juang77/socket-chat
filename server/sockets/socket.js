ef0714edf6ffd53c6fa0bce607bab2686b3c4190
Juan Gomez <juang77@hotmail.com>
2020-06-20 17:38:43 -0500

    Primer Commit

---
+++ server/sockets/socket.js
@@ -0,0 +1,61 @@
+const { io } = require('../server');
+const { Usuarios } = require('../classes/usuarios');
+const { crearMensaje } = require('../utilidades/utilidades');
+
+const usuarios = new Usuarios();
+
+io.on('connection', (client) => {
+
+    client.on('entrarChat', (usuario, callback) => {
+
+        if (!usuario.nombre || !usuario.sala) {
+            return callback({
+                error: true,
+                message: 'El nombre y la sala son requeridos.'
+            });
+        }
+
+        client.join(usuario.sala);
+
+        usuarios.agregarPersona(client.id, usuario.nombre, usuario.sala);
+
+        client.broadcast.to(usuario.sala).emit('listaPersonas', usuarios.getPersonasPorSala(usuario.sala));
+
+        callback(usuarios.getPersonasPorSala(usuario.sala));
+
+    });
+
+    client.on('crearMensaje', (data) => {
+
+        let persona = usuarios.getPersona(client.id);
+
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+
+    });
+
+    client.on('disconnect', () => {
+
+        let personaBorrada = usuarios.borrarPersona(client.id);
+
+        client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${ personaBorrada.nombre} abandonó el Chat.`));
+        client.broadcast.to(personaBorrada.sala).emit('listaPersonas', usuarios.getPersonasPorSala(personaBorrada.sala));
+
+    });
+
+    client.on('mensajePrivado', (data) => {
+        if (!client.id) {
+            return json({
+                error: true,
+                message: 'El Id es requerido.'
+            })
+        }
+
+        let persona = usuarios.getPersona(client.id)
+
+        client.broadcast.to(data.para).emit('mensajePrivado', crearMensaje(persona.nombre, data.mensaje));
+
+
+    })
+
+});
